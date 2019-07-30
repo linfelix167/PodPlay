f2783c5e9ce60b56869277b4244f4863904d6fd8
Felix Lin <swift.python.geeks@gmail.com>
2019-07-30 01:12:48 -0700

    Defining Retrofit dependencies

--- app/src/main/java/com/felix/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/felix/podplay/ui/PodcastActivity.kt
@@ -2,12 +2,25 @@ package com.felix.podplay.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.felix.podplay.R
+import com.felix.podplay.repository.ItunesRepo
+import com.felix.podplay.service.ItunesService
 
 class PodcastActivity : AppCompatActivity() {
+
+    val TAG = javaClass.simpleName
+
+    val itunesService = ItunesService.instance
+
+    val itunesRepo = ItunesRepo(itunesService)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
+
+        itunesRepo.searchByTerm("Android Developer") {
+            Log.i(TAG, "Results = $it")
+        }
     }
 }
