d04bfe57aabe24c1e48d8e2376377215cb4dc73e
Felix Lin <swift.python.geeks@gmail.com>
2019-08-01 14:44:09 -0700

    Loading the options menu

--- app/src/main/java/com/felix/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/felix/podplay/ui/PodcastActivity.kt
@@ -1,8 +1,12 @@
 package com.felix.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 import com.felix.podplay.R
 import com.felix.podplay.repository.ItunesRepo
 import com.felix.podplay.service.ItunesService
@@ -22,5 +26,19 @@ class PodcastActivity : AppCompatActivity() {
         itunesRepo.searchByTerm("Android Developer") {
             Log.i(TAG, "Results = $it")
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+
+        val searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return true
     }
 }
